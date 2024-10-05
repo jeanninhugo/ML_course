30f7a73ed74c7fa271b43f3bbd78b6731f4fdce8
hugo jeannin <hugo.jeannin@epfl.ch>
2024-10-05 15:25:20 +0200

    ex03 finished

--- labs/ex02/template/grid_search.py
+++ labs/ex02/template/grid_search.py
@@ -21,6 +21,49 @@ def get_best_parameters(w0, w1, losses):
     return losses[min_row, min_col], w0[min_row], w1[min_col]
 
 
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+
+    n = y.shape[0]
+    loss = (0.5 / n) * np.sum((y - w[0] - w[1]*tx[:,1]) ** 2)
+    
+    return loss
+
+
+
+def gs_algo(y, tx, grid_w0, grid_w1):
+    """Algorithm for grid search.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        grid_w0: numpy array of shape=(num_grid_pts_w0, ). A 1D array containing num_grid_pts_w0 values of parameter w0 to be tested in the grid search.
+        grid_w1: numpy array of shape=(num_grid_pts_w1, ). A 1D array containing num_grid_pts_w1 values of parameter w1 to be tested in the grid search.
+
+    Returns:
+        losses: numpy array of shape=(num_grid_pts_w0, num_grid_pts_w1). A 2D array containing the loss value for each combination of w0 and w1
+    """
+
+    losses = np.zeros((len(grid_w0), len(grid_w1)))
+
+    for i0, w0 in enumerate(grid_w0):
+        for i1, w1 in enumerate(grid_w1):
+            losses[i0][i1] = compute_loss(y, tx, np.array([w0, w1]))
+    
+    return losses
+
+
+
+
 # ***************************************************
 # INSERT YOUR CODE HERE
 # TODO: Paste your implementation of grid_search
--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -12,4 +12,6 @@ def build_poly(x, degree):
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
     # ***************************************************
+    poly = np.column_stack([x**d for d in range(degree + 1)])
+    return poly
     raise NotImplementedError
